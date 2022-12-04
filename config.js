c050594215874c26d192a222ec05785a27f27473
blenk01 <morel.t@outlook.fr>
2022-12-05 00:18:30 +0100

    Change API_URL to call ipv4 address in config.js file

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const STRIPE_PK = "pk_test_51LvDY9E9rLHl2YN3yJpHc0iXKRaY5oS9tVUm5FG9Y0iuFS0ZCSG33o2eLNUQH8cchsiTvfOlEVxqKPH6pByAdtqg00SkFo7lWp";
-export const API_URL = "https://localhost:8003";
+export const API_URL = "http://192.168.0.26:8003";
+export const PREFIX_BARCODE='item_id=';
