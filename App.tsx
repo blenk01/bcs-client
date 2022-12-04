770ae2a1520008fb8c9c3e5ed462273d5fa4fbc6
blenk01 <morel.t@outlook.fr>
2022-12-04 23:26:16 +0100

    Set StackNavigator and add ScannerScreen to App component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import { StripeProvider } from '@stripe/stripe-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CheckoutScreen from './CheckoutScreen';
 import {STRIPE_PK} from './config';
+import ScannerScreen from './ScannerScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <StripeProvider
+    /*<StripeProvider
       publishableKey={STRIPE_PK}
       merchantIdentifier="merchant.com.example"
     >
       <CheckoutScreen />
-    </StripeProvider>
+    </StripeProvider>*/
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="scanner" component={ScannerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
