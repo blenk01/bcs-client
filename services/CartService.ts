8fb782a39edfca2b65e91f9931e2a7aeb9a55b6b
blenk01 <morel.t@outlook.fr>
2022-12-05 17:14:30 +0100

    Replace all types by ItemCart

--- services/CartService.ts
+++ services/CartService.ts
@@ -1,5 +1,6 @@
 import * as SecureStore from 'expo-secure-store';
 import { CART_KEY } from '../config';
+import { ItemCart } from '../types/itemCart';
 import ApiService from './ApiService';
 
 let cart: { [key: string]: number } = {};
@@ -29,11 +30,11 @@ export default {
         return saveCart();
     },
     getItems: async () => {
-        const items: {id: string, name: string, price: number, inCart: number}[] = [];
+        const items: ItemCart[] = [];
         for ( const itemId in cart ) {
             const item: {id: string, name: string, price: number} = await ApiService.getItem(itemId);
             if ( item ) {
-                const itemData: {id: string, name: string, price: number, inCart: number} = {
+                const itemData: ItemCart = {
                     ...item,
                     inCart: cart[itemId]
                 };
