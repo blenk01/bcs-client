13df7ddcd792f95aa785ae6bf93f8e3304b1129f
blenk01 <morel.t@outlook.fr>
2022-12-05 20:46:24 +0100

    Add getPurchaseHistory method in API service

--- services/ApiService.ts
+++ services/ApiService.ts
@@ -19,4 +19,22 @@ export default {
 
         return data;
     },
+    getPurchaseHistory: async (customerId: number) => {
+      let response;
+      try {
+        response = await fetch(`${API_URL}/payments/${customerId}`, {
+          method: 'GET',
+        });
+      } catch(e) {
+        throw 'A server error has occurred !';
+      }
+
+      const data = await response.json();
+
+      if ( !response.ok ) {
+          throw 'Server send not ok !';
+      } 
+
+      return data;
+    }
 };
