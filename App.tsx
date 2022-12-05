f22805e568ddc4f1485d67753193c785ff697c29
blenk01 <morel.t@outlook.fr>
2022-12-05 17:05:09 +0100

    Add decreaseQuantity and getItems in cart service

--- App.tsx
+++ App.tsx
@@ -4,20 +4,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CheckoutScreen from './CheckoutScreen';
 import {STRIPE_PK} from './config';
 import ScannerScreen from './ScannerScreen';
+import ShoppingCartScreen from './ShoppingCartScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    /*<StripeProvider
-      publishableKey={STRIPE_PK}
-      merchantIdentifier="merchant.com.example"
-    >
-      <CheckoutScreen />
-    </StripeProvider>*/
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="scanner" component={ScannerScreen} />
+        <Stack.Screen name="shoppingCart" component={ShoppingCartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
