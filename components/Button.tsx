91b14c71d9b395907b1c0a69e3cddbc21089e19f
blenk01 <morel.t@outlook.fr>
2022-12-05 22:14:53 +0100

    Create reusable button component

---
+++ components/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+
+export default function Button(props: {onPress: any, title: string}) {
+  const { onPress, title } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    marginBottom: 1,
+    marginTop: 1,
+    borderTopEndRadius: 10,
+    borderBottomStartRadius: 10,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
